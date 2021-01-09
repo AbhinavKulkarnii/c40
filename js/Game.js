3556124ac9044a47f2c19d228087e9c36acfc5f0
Abhinav Kulkarni <Abhinav@kulkarnis-mini.attlocal.net>
2021-01-09 13:40:54 -0600

    added images to the car objects

--- js/Game.js
+++ js/Game.js
@@ -22,31 +22,52 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100,200);
+    car1.addImage("car1",car1img);
+    car2 = createSprite(300,200);
+    car2.addImage("car2",car2img);
+    car3 = createSprite(500,200);
+    car3.addImage("car3",car3img);
+    car4 = createSprite(700,200);
+    car4.addImage("car4",car4img);
+    cars = [car1,car2,car3,car4];
   }
   
   play(){
     form.hide()
-    textSize(30)
-    text("Game Started",120,100);
     Player.getPlayerInfo();
     if(allPlayers !== undefined){
-      var yPos = 130
+      background(groundImg);
+      image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5)
+      var index = 0
+      var x = 180
+      var y 
       for(var plr in allPlayers){
-        if(plr === "player"+player.index)
-        fill("red");
-        else 
-        fill("black");
-        yPos += 20
-        textSize(15)
-        text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,yPos);
+        index = index+1
+        x = x+200
+        y = displayHeight - allPlayers[plr].distance
+        cars[index-1].x = x
+        cars[index-1].y = y
+        if(index === player.index){
+          cars[index-1].shapeColor = "red"
+          camera.position.x = displayWidth/2
+          camera.position.y = cars[index-1].y
+        }
       }
     }
     if(keyIsDown(UP_ARROW)&& player.index !== null){
       player.distance += 50
       player.update();
     }
+    if(player.distance > 3860){
+      gameState = 2;
+    }
+    drawSprites();
   }
 
+    end(){
+      console.log("game ended")
+    }
 
 
 }
--- sketch.js
+++ sketch.js
@@ -4,14 +4,28 @@ var playerCount;
 var database;
 var form, player, game;
 var allPlayers
+var car1, car2, car3, car4
+var car1img, car2img, car3img, car4img
+var groundImg
+var trackImg
+var cars
 
+function preload(){
+  car1img = loadImage("images/car1.png");
+  car2img = loadImage("images/car2.png");
+  car3img = loadImage("images/car3.png");
+  car4img = loadImage("images/car4.png");
+  trackImg = loadImage("images/track.jpg");
+  groundImg = loadImage("images/ground.png");
+}
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
+
 }
 
 function draw(){
@@ -21,6 +35,10 @@ function draw(){
   if(gameState === 1){
     clear()
     game.play();
+  }
+
+  if(gameState === 2){
+    game.end();
   }
   
 }
